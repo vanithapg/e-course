24ce1ce18fac5cb02b15eef3697c9d8c84e2a4f7
Vanitha <vanithapg@gmail.com>
2021-05-24 00:23:44 -0500

    redux-form

--- src/Support.js
+++ src/Support.js
@@ -1,29 +1,45 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class Support extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit}>
+const Support = props => {
+  const { handleSubmit, submitting } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First Name</label>
         <div>
-          <label htmlFor="name">Name</label>
-          <Field name="name" component="input" type="text"/>
+          <Field
+            name="firstName"
+            component="input"
+            type="text"
+            placeholder="First Name"
+          />
         </div>
+      </div>
+      <div>
+        <label>Last Name</label>
         <div>
-          <label htmlFor="desc">Enter your Query</label>
-          <Field name="desc" component="input" type="textarea"/>
+          <Field
+            name="lastName"
+            component="input"
+            type="text"
+            placeholder="Last Name"
+          />
         </div>
+      </div>
+      <div>
+        <label>Enter your Enquiry</label>
+        <div>
+          <Field name="enquiry" component="textarea" />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={submitting}>Submit</button>
+      </div>
+    </form>
+  );
+};
 
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
-}
-
-Support = reduxForm({
-  form: 'support'
+export default reduxForm({
+  form: 'support',
 })(Support);
-
-export default Support;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./store";
 
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-
-const reducers = {
-  form: formReducer 
-}
-const reducer = combineReducers(reducers)
-const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
